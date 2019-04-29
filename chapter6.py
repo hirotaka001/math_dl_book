2fbdff3eae3c5a8d5a91104425edc8eb3bdf8fe3
kacpj-pc01 <>
2019-04-29 20:56:34 +0900

    Add maximum_likelihood_estimation in chapter6.py

--- chapter6.py
+++ chapter6.py
@@ -22,6 +22,7 @@ def normal(x):
     return np.exp(-((x-500)**2)/500)/np.sqrt(500*np.pi)
 print(integrate.quad(normal, 0, 480))
 
+## 正規分布関数とシグモイド関数
 # 正規分布関数
 def std(x, sigma=1):
     return (np.exp(-(x/sigma)**2/2)) / (np.sqrt(2*np.pi)*sigma)
@@ -43,3 +44,22 @@ plt.plot(x, y_sig, label="sig", c='b', lw=3)
 plt.legend(fontsize=14)
 plt.grid(lw=2)
 plt.savefig('gause_sigmoid.png')
+
+## 最尤推定
+def L(p, n, k):
+    return ((p**k) * ((1-p)**(n-k)))
+
+x = np.linspace(0, 1, 1000)
+y = L(x, 5, 2)
+x0 = np.asarray([0.4, 0.4])
+y0 = np.asarray([0, L(0.4, 5, 2)])
+
+plt.figure((figsize=(6, 6))
+plt.plot(x, y, c='b', lw=3)
+plt.plot(x0, y0, linestyle='dashed', c='k', lw=3)
+plt.xticks(size=16)
+plt.yticks(size=16)
+plt.grid(lw=2)
+plt.xlabel("p", fontsize=16)
+plt.ylabel("L(p)", fontsize=16)
+plt.savefig('maximum_likelihood_estimation.png')
