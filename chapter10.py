ff8f11e5b899d0a1848d6612bc2adedecd0b988b
ha-takahashi <ha-takahashi@kddi.com>
2019-11-10 00:39:20 +0900

    Modify chapter10.py

--- chapter10.py
+++ chapter10.py
@@ -9,6 +9,7 @@ from IPython.display import set_matplotlib_formats
 from sklearn.datasets import fetch_mldata
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 # sys.stdoutのエンコード変更
 enc = 'utf-8'
@@ -72,3 +73,482 @@ for i in range(N):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 plt.savefig('data_chapter10.png')
+
+# 予測関数
+# シグモイド関数
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+# softmax関数
+def softmax(x):
+    x = x.T
+    x_max = x.max(axis=0)
+    x = x - x_max
+    w = np.exp(x)
+    return (w / w.sum(axis=0)).T
+
+# 評価関数
+# 交差エントロピー関数
+def cross_entropy(yt, yp):
+    return -np.mean(np.sum(yt * np.log(yp), axis=1))
+
+# 評価処理(戻り値は精度と損失関数)
+def evaluate(x_test, y_test, y_test_one, V, W):
+    b1_test = np.insert(sigmoid(x_test @ V), 0, 1, axis=1)
+    yp_test_one = softmax(b1_test @ W)
+    yp_test = np.argmax(yp_test_one, axis=1)
+    loss = cross_entropy(y_test_one, yp_test_one)
+    score = accuracy_score(y_test, yp_test)
+    return score, loss
+
+# ミニバッチ処理
+# ミニバッチ処理index取得関数
+class Indexes():
+    # コンストラクタ
+    def __init__(self, total, size):
+        # 配列全体の大きさ
+        self.total = total
+        # batchサイズ
+        self.size = size
+        # 作業用indexes 初期値はNULLにしておく
+        self.indexes = np.zeros(0)
+
+    # index取得関数
+    def next_index(self):
+        next_flag = False
+
+        # batchサイズより作業用indexesが小さい場合はindexes再生成
+        if len(self.indexes) < self.size:
+            self.indexes = np.random.choice(self.total, self.total, replace=False)
+            next_flag = True
+
+        # 戻り用index取得と作業用indexes更新
+        index = self.indexes[:self.size]
+        self.indexes = self.indexes[self.size:]
+        return index, next_flag
+
+# indexesクラスのテスト
+
+# クラス初期化
+# 20:全体の配列の大きさ
+# 5:一回に取得するindexの数
+indexes = Indexes(20, 5)
+
+for i in range(6):
+    # next_index関数呼び出し
+    # 戻り値1: indexのnumpy配列
+    # 戻り値2: 作業用Indexの更新があったかどうか
+    arr, flag = indexes.next_index()
+    print(arr, flag)
+
+
+# 初期化処理 その1
+# 変数初期宣言 初期バージョン
+
+# 隠れ層のノード数
+H = 128
+H1 = H + 1
+# M:訓練用系列データ総数
+M = x_train.shape[0]
+# D:入力データ次元数
+D = x_train.shape[1]
+# N:分類クラス数
+N = y_train_one.shape[1]
+
+# 繰り返し回数
+nb_epoch = 100
+# ミニバッチサイズ
+batch_size = 512
+B = batch_size
+# 学習率
+alpha = 0.01
+
+# 重み行列の初期設定(すべて1)
+V = np.ones((D, H))
+W = np.ones((H1, N))
+
+# 評価結果記録用(損失関数値と精度)
+history1 = np.zeros((0, 3))
+
+# ミニバッチ用関数初期化
+indexes = Indexes(M, batch_size)
+
+# 繰り返し関数カウンタ初期化
+epoch = 0
+
+# メイン処理
+while epoch < nb_epoch:
+    # 学習対象の選択(ミニバッチ学習法)
+    index, next_flag = indexes.next_index()
+    x, yt = x_train[index], y_train_one[index]
+
+    # 予測値計算(順伝播)
+    a = x @ V                           # (10.6.3)
+    b = sigmoid(a)                      # (10.6.4)
+    b1 = np.insert(b, 0, 1, axis=1)     # ダミー変数の追加
+    u = b1 @ W                          # (10.6.5)
+    yp = softmax(u)                     # (10.6.6)
+
+    # 誤差計算
+    yd = yp - yt                        # (10.6.7)
+    bd = b * (1 - b) * (yd @ W[1:].T)   # (10.6.8)
+
+    # 勾配計算
+    W = W - alpha * (b1.T @ yd) / B     # (10.6.9)
+    V = V - alpha * (x.T @ bd) / B      # (10.6.10)
+
+    # ログ記録用
+    if next_flag:                       # 1epoch終了後の処理
+        score, loss = evaluate(x_test, y_test, y_test_one, V, W)
+        history1 = np.vstack((history1, np.array([epoch, loss, score])))
+        print("epoch=%d loss=%f score=%f" % (epoch, loss, score))
+        epoch = epoch + 1
+
+# 結果確認　その１
+# 損失関数値と精度の確認
+print('初期状態: 損失関数:%f 精度:%f' % (history1[0, 1], history1[-1, 2]))
+print('最終状態: 損失関数:%f 精度:%f' % (history1[-1, 1], history1[-1, 2]))
+
+# 学習曲線の表示(損失関数値)
+plt.plot(history1[:, 0], history1[:, 1])
+plt.ylim(0, 2.5)
+plt.xticks(size=14)
+plt.yticks(size=14)
+plt.grid(lw=2)
+plt.savefig('learning_curve_loss1.png')
+
+# 学習曲線の表示(精度)
+plt.plot(history1[:, 0], history1[:, 1])
+plt.ylim(0, 2.5)
+plt.xticks(size=14)
+plt.yticks(size=14)
+plt.grid(lw=2)
+plt.savefig('learning_curve_accuracy1.png')
+
+## 10.8 パラメータ初期値の設定変更
+# 変数初期宣言 重み行列の初期化方式変更
+
+# 隠れ層のノード
+H = 128
+H1 = H + 1
+
+# M:訓練用系列データ総数
+M = x_train.shape[0]
+
+# D:入力データ次元数
+D = x_train.shape[1]
+
+# N:分類クラス数
+N = y_train_one.shape[1]
+
+# 機械学習パラメータ
+alpha = 0.01
+nb_epoch = 100
+batch_size = 512
+B = batch_size
+
+# 重み行列の初期設定(すべて1)
+V = np.ones((D, H))
+W = np.ones((H1, N))
+
+# 評価結果記録用(損失関数値と精度)
+history2 = np.zeros((0, 3))
+
+# ミニバッチ用関数初期化
+indexes = Indexes(M, batch_size)
+
+# 繰り返し回数カウンタ初期化
+epoch = 0
+
+# 重み行列の初期化設定改訂版
+V = np.random.randn(D, H) / np.sqrt(D / 2)
+W = np.random.randn(H1, N) / np.sqrt(H1 /2)
+print(V[:2, :5])
+print(W[:2, :5])
+
+# メイン処理
+while epoch < nb_epoch:
+    # 学習対象の選択(ミニバッチ学習法)
+    index, next_flag = indexes.next_index()
+    x, yt = x_train[index], y_train_one[index]
+
+    # 予測値計算(順伝播)
+    a = x @ V                           # (10.6.3)
+    b = sigmoid(a)                      # (10.6.4)
+    b1 = np.insert(b, 0, 1, axis=1)     # ダミー変数の追加
+    u = b1 @ W                          # (10.6.5)
+    yp = softmax(u)                     # (10.6.6)
+
+    # 誤差計算
+    yd = yp - yt                        # (10.6.7)
+    bd = b * (1-b) * (yd @ W[1:].T)     # (10.6.8)
+
+    # 勾配計算
+    W = W - alpha * (b1.T @ yd) / B     # (10.6.9)
+    V = V - alpha * (x.T @ bd) / B      # (10.6.10)
+
+    if next_flag:                       # 1epoch終了後の処理
+        score, loss = evaluate(x_test, y_test, y_test_one, V, W)
+        history2 = np.vstack((history2, np.array([epoch, loss, score])))
+        print("epoch=%d loss=%f score=%f" % (epoch, loss, score))
+        epoch = epoch + 1
+
+# 結果確認 その2
+# 損失関数値と精度の確認
+print('初期状態: 損失関数:%f 精度:%f' % (history2[0, 1], history2[0, 2]))
+print('最終状態: 損失関数:%f 精度:%f' % (history2[-1, 1], history2[-1, 2]))
+
+# 学習曲線の表示(損失関数値)
+plt.plot(history2[:, 0], history2[:, 1])
+plt.ylim(0, 2.5)
+plt.xticks(size=14)
+plt.yticks(size=14)
+plt.grid(lw=2)
+plt.savefig('learning_curve_loss2.png')
+
+# 学習曲線の表示(精度)
+plt.plot(history2[:, 0], history2[:, 2])
+plt.ylim(0, 1)
+plt.xticks(size=14)
+plt.yticks(size=14)
+plt.grid(lw=2)
+plt.savefig('learning_curve_accuracy2.png')
+
+## 10.9 ReLU関数の導入
+# ReLU関数
+def ReLU(x):
+    return np.maximum(0, x)
+# step関数
+def step(x):
+    return 1.0 * (x > 0)
+
+# ReLU関数とstep関数のグラフ表示
+xx = np.linspace(-4, 4, 501)
+yy = ReLU(xx)
+plt.figure(figsize=(6, 6))
+#plt.ylim(0.0, 1.0)
+plt.xlim(-4, 4)
+plt.ylim(-4, 4)
+plt.xlabel(r'$x$', fontdict=14)
+plt.ylabel(r'$y$', fontdict=14)
+plt.grid(lw=2)
+plt.plot(xx, ReLU(xx), c='b', label='ReLU', linestyle='-', lw=3)
+plt.plot(xx, step(xx), c='k', lagel='step', linestyle='-', lw=3)
+plt.xticks(size=14)
+plt.yticks(size=14)
+plt.legend(fontsize=14)
+plt.savefig('ReLU_step.png')
+
+# 評価 その2
+def evaluate2(x_test, y_test, y_test_one, V, W):
+    b1_test = np.insert(ReLU(x_test @ V), 0, 1, axis=1)
+    yp_test_one = softmax(b1_test @ W)
+    yp_test = np.argmax(yp_test_one, axis=1)
+    loss = cross_entropy(y_test_one, yp_test_one)
+    score = accuracy_score(y_test, yp_test)
+    return score, loss
+
+# 初期化処理
+# 変数初期化宣言 重み行列の初期化方式変更
+# 隠れ層のノード数
+H = 128
+H1 = H + 1
+# M:訓練用系列データ総数
+M = x_train.shape[0]
+
+# D:入力データ次元数
+D = x_train.shape[1]
+
+# N:分類クラス数
+N = y_train_one.shape[1]
+
+# 機械学習パラメータ
+alpha = 0.01
+nb_epoch = 100
+batch_size = 512
+B = batch_size
+
+# 重み行列の初期設定
+V = np.random.randn(D, H) / np.sqrt(D / 2)
+W = np.random.randn(H1, N) / np.sqrt(H1 / 2)
+
+# 評価結果記録用(損失関数値と精度)
+history3 = np.zeros((0, 3))
+
+# ミニバッチ用関数初期化
+indexes = Indexes(M, batch_size)
+
+# 繰り返し回数カウンタ初期化
+
+# メイン処理(シグモイド関数をLeRU関数に変更)
+while epoch < nb_epoch:
+    # 学習対象の選択(ミニバッチ学習用)
+    index, next_flag = indexes.next_index()
+    x, yt = x_train[index], y_train_one[index]
+
+    # 予測値計算(順伝播)
+    a = x @ V                           # (10.6.3)
+    b = ReLU(a)                         # (10.6.4) ReLU化
+    b1 = np.insert(b, 0, 1, axis=1)     # ダミー変数の追加
+    u = b1 @ W                          # (10.6.5)
+    yp = softmax(u)                     # (10.6.6)
+
+    # 誤差計算
+    yd = yp - yt                        # (10.6.7)
+    bd = step(a) * (yd @ W[1:].T)       # (10.6.8) ReLU化
+
+    # 勾配計算
+    W = W - alpha * (b1.T @ yd) / B     # (10.6.9)
+    V = V - alpha * (x.T @ bd) / B      # (10.6.10)
+
+    if next_flag:                       # 1epoch終了後の処理
+        score, loss = evaluate2(x_test, y_test, y_test_one, V, W)
+        history3 = np.vstack((history3, np.array([epoch, loss, score])))
+        print("epoch=%d loss=%f score=%f" % (epoch, loss, score))
+        epoch = epoch + 1
+
+# 結果確認 その3
+# 損失関数値と精度の確認
+print('初期状態: 損失関数:%f 精度:%f' % (history3[0, 1], history3[0, 2]))
+print('最終状態: 損失関数:%f 精度:%f' % (history3[-1, 1], history3[-1, 2]))
+
+# 学習曲線の表示(損失関数値)
+plt.plot(history3[:, 0], history3[:, 1])
+plt.ylim(0, 2.5)
+plt.xticks(size=14)
+plt.yticks(size=14)
+plt.grid(lw=2)
+plt.savefig('learning_curve_loss3.png')
+
+# 学習曲線の表示(精度)
+plt.plot(history3[:, 0], history3[:, 2])
+plt.ylim(0, 1)
+plt.xticks(size=14)
+plt.yticks(size=14)
+plt.grid(lw=2)
+plt.savefig('learning_curve_accuracy3.png')
+
+# データ内容の確認
+N = 20
+np.random.seed(123)
+indexes = np.random.choice(y_test.shape[0], N, replace=False)
+
+# x_orgの選択結果表示(白黒反転)
+x_selected = x_test[indexes]
+y_selected = y_test[indexes]
+
+# 予測値の計算
+b1_test = np.insert(ReLU(x_selected @ V), 0, 1, axis=1)
+yp_test_one = softmax(b1_test @ W)
+yp_test = np.argmax(yp_test_one, axis=1)
+
+# グラフ表示
+plt.figure(figsize=(10, 3))
+for i in range(N):
+    ax = plt.subplot(2, N/2, i+1)
+    plt.imshow(x_selected[i, 1:].reshape(28, 28), cmap='gray_r')
+    ax.set_title('%d:%d' % (y_selected[i], yp_test[i]), fontsize=14)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.savefig('graph1.png')
+
+## 10.10 隠れ層2階層化
+# 評価処理(隠れ層2階層対応版)
+def evaluate3(x_test, y_test, y_test_one, U, V, W):
+    b1_test = np.insert(ReLU(x_test @ U), 0, 1, axis=1)
+    d1_test = np.insert(ReLU(b1_test @ V), 0, 1, axis=1)
+    yp_test_one = softmax(d1_test @ W)
+    yp_test = np.argmax(yp_test_one, axis=1)
+    loss = cross_entropy(y_test_one, yp_test_one)
+    score = accuracy_score(y_test, yp_test)
+    return score, loss
+
+# 初期化処理 その3
+# 変数初期宣言 重み行列の初期化方式変更
+# 隠れ層のノード数
+H = 128
+H1 = H + 1
+
+# M:訓練用系列データ総数
+M = x_train.shape[0]
+
+# N:分類クラス数
+N = y_train_one.shape[1]
+
+# 機械学習パラメータ
+alpha = 0.01
+nb_epoch = 200
+batch_size = 512
+B = batch_size
+
+# 重み行列の初期設定
+U = np.random.randn(D, H) / np.sqrt(D / 2)
+V = np.random.randn(H1, H) / np.sqrt(H1 / 2)
+W = np.random.randn(H1, N) / np.sqrt(H1 / 2)
+
+# 評価結果記録用(損失関数値と精度)
+history4 = np.zeros((0, 3))
+
+# ミニバッチ用関数初期化
+indexes = Indexes(M, batch_size)
+
+# 繰り返し回数カウンタ初期化
+epoch = 0
+
+# メイン処理 その4
+# メイン処理(隠れ層2層化)
+while epoch < nb_epoch:
+    # 学習対象の選択(ミニバッチ学習法)
+    index, next_flag = indexes.next_index()
+    x, yt = x_train[index], y_train_one[index]
+
+    # 予測値計算(順伝播)
+    a = x @ U                               # (10.6.11)
+    b = ReLU(a)                             # (10.6.12)
+    b1 = np.insert(b, 0, 1, axis=1)         # ダミー変数の追加
+    c = b1 @ V                              # (10.6.13)
+    d = ReLU(c)                             # (10.6.14)
+    d1 = np.insert(d, 0, 1, axis=1)         # ダミー変数の追加
+    u = d1 @ W                              # (10.6.15)
+    yp = softmax(u)                         # (10.6.16)
+
+    # 誤差計算
+    yd = yp - yt                            # (10.6.17)
+    dd = step(c) * (yd @ W[1:].T)           # (10.6.18)
+    bd = step(a) * (dd @ V[1:].T)           # (10.6.19)
+
+    # 勾配計算
+    W = W - alpha * (d1.T @ yd) / B         # (10.6.20)
+    V = V - alpha * (b1.T @ dd) / B         # (10.6.21)
+    U = U - alpha * (x.T @ bd) / B          # (10.6.22)
+
+    if next_flag:                           # 1epoch 終了後の処理
+        score, loss = evaluate3(x_test, y_test, y_test_one, U, V, W)
+        history4 = np.vstack((history4, np.array([epoch, loss, score])))
+        print("epoch=%d loss=%f score=%f" % (epoch, loss, score))
+        epoch = epoch + 1
+
+# データ内容の確認
+N = 20
+np.random.seed(123)
+indexes = np.random.choice(y_test.shape[0], N, replace=False)
+
+# x_orgの選択結果表示(白黒反転)
+x_selected = x_test[indexes]
+y_selected = y_test[indexes]
+
+# 予測値の計算
+b1_test = np.insert(ReLU(x_selected @ U), 0, 1, axis=1)
+d1_test = np.insert(ReLU(b1_test @ V), 0, 1, axis=1)
+yp_test_one = softmax(d1_test @ W)
+yp_test = np.argmax(yp_test_one, axis=1)
+
+# グラフ表示
+plt.figure(figsize=(10, 3))
+for i in range(N):
+    ax = plt.subplot(2, N/2, i+1)
+    plt.imshow(x_selected[i, 1:].reshape(28, 28), cmap='gray_r')
+    ax.set_title('%d:%d' % (y_selected[i], yp_test[i]), fontsize=14)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.savefig('graph2.png')
